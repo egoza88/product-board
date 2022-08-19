1b9761c13e4b6142e74fd21177d6abb9454f6f25
Yehor Petukhov <yehor@Yehors-MacBook-Pro.local>
2022-08-19 22:49:57 +0200

    Init commit

---
+++ src/main/java/com/product/board/productboardassignment/ProductBoardAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.product.board.productboardassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductBoardAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductBoardAssignmentApplication.class, args);
+	}
+
+}
